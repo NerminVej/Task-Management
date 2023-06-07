fa7b56183e39a4c895b1feb6cd6ab6584c79ecbd
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-07 21:24:28 +0200

    added tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import RegistrationPage from "./RegistrationPage";
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <RegistrationPage></RegistrationPage>
     </div>
   );
 }
---
+++ src/RegistrationPage.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+const RegistrationPage = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission and validation here
+  };
+
+  return (
+    <div className="container mx-auto px-4">
+      <form onSubmit={handleFormSubmit}>
+        <div className="mt-4">
+          <label htmlFor="name" className="block font-medium text-gray-700">Name</label>
+          <input
+            id="name"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+          />
+        </div>
+        <div className="mt-4">
+          <label htmlFor="email" className="block font-medium text-gray-700">Email</label>
+          <input
+            id="email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+          />
+        </div>
+        <div className="mt-4">
+          <label htmlFor="password" className="block font-medium text-gray-700">Password</label>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+          />
+        </div>
+        <button
+          type="submit"
+          className="mt-4 inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default RegistrationPage;
