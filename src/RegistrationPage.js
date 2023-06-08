d694cbaedf8b9bdbd62faf14dbc61e440018f39a
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-08 08:36:52 +0200

    added styling changes

--- src/RegistrationPage.js
+++ src/RegistrationPage.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const RegistrationPage = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
@@ -11,10 +11,12 @@ const RegistrationPage = () => {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      <form onSubmit={handleFormSubmit}>
-        <div className="mx-auto w-1/3 bg-gray-200 p-4">
-          <label htmlFor="name" className="block font-medium text-gray-700">Name</label>
+    <form onSubmit={handleFormSubmit}>
+      <div className="w-6">
+        <div className="mb-4">
+          <label htmlFor="name" className="block font-medium text-gray-700">
+            Name
+          </label>
           <input
             id="name"
             type="text"
@@ -22,8 +24,12 @@ const RegistrationPage = () => {
             onChange={(e) => setName(e.target.value)}
             className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
           />
-        
-          <label htmlFor="email" className="block mt-4 font-medium text-gray-700">Email</label>
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="email" className="block font-medium text-gray-700">
+            Email
+          </label>
           <input
             id="email"
             type="email"
@@ -31,8 +37,12 @@ const RegistrationPage = () => {
             onChange={(e) => setEmail(e.target.value)}
             className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
           />
-        
-          <label htmlFor="password" className="block mt-4 font-medium text-gray-700">Password</label>
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="password" className="block font-medium text-gray-700">
+            Password
+          </label>
           <input
             id="password"
             type="password"
@@ -40,16 +50,16 @@ const RegistrationPage = () => {
             onChange={(e) => setPassword(e.target.value)}
             className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
           />
-
-          <button
-            type="submit"
-            className="mt-4 inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            Sign Up
-          </button>
         </div>
-      </form>
-    </div>
+
+        <button
+          type="submit"
+          className="mt-4 inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          Sign Up
+        </button>
+      </div>
+    </form>
   );
 };
 
