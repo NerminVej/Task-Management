6f64b29853c19498e64510ab2af20a31fa2a8558
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-08 08:20:40 +0200

    added some styling changes

--- src/RegistrationPage.js
+++ src/RegistrationPage.js
@@ -11,9 +11,9 @@ const RegistrationPage = () => {
   };
 
   return (
-    <div className="container mx-auto px-4">
+    <div className="flex items-center justify-center h-screen">
       <form onSubmit={handleFormSubmit}>
-        <div className="mt-4">
+        <div className="mx-auto w-1/3 bg-gray-200 p-4">
           <label htmlFor="name" className="block font-medium text-gray-700">Name</label>
           <input
             id="name"
@@ -22,9 +22,8 @@ const RegistrationPage = () => {
             onChange={(e) => setName(e.target.value)}
             className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
           />
-        </div>
-        <div className="mt-4">
-          <label htmlFor="email" className="block font-medium text-gray-700">Email</label>
+        
+          <label htmlFor="email" className="block mt-4 font-medium text-gray-700">Email</label>
           <input
             id="email"
             type="email"
@@ -32,9 +31,8 @@ const RegistrationPage = () => {
             onChange={(e) => setEmail(e.target.value)}
             className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
           />
-        </div>
-        <div className="mt-4">
-          <label htmlFor="password" className="block font-medium text-gray-700">Password</label>
+        
+          <label htmlFor="password" className="block mt-4 font-medium text-gray-700">Password</label>
           <input
             id="password"
             type="password"
@@ -42,13 +40,14 @@ const RegistrationPage = () => {
             onChange={(e) => setPassword(e.target.value)}
             className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
           />
+
+          <button
+            type="submit"
+            className="mt-4 inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Sign Up
+          </button>
         </div>
-        <button
-          type="submit"
-          className="mt-4 inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        >
-          Sign Up
-        </button>
       </form>
     </div>
   );
