4a4549190bd3bc5a00fd91832cf62cba240efef5
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-08 09:38:02 +0200

    added button style

--- src/RegistrationPage.js
+++ src/RegistrationPage.js
@@ -12,9 +12,7 @@ const RegistrationPage = () => {
 
   return (
     <div>
-      <button className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md">
-        sdf
-      </button>
+      <button className="btn">fdsfdsfsd</button>
     </div>
   );
   
