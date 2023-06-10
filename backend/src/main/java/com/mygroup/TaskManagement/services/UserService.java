ffbc6222945bec03a75b146ab2016f369cf4ef30
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-10 12:44:50 +0200

    added service annotation

--- backend/src/main/java/com/mygroup/TaskManagement/services/UserService.java
+++ backend/src/main/java/com/mygroup/TaskManagement/services/UserService.java
@@ -2,13 +2,19 @@ package com.mygroup.TaskManagement.services;
 
 import com.mygroup.TaskManagement.models.User;
 import com.mygroup.TaskManagement.repositories.UserRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class UserService {
 
     private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public List<User> getAllUsers(){
         return userRepository.findAll();
