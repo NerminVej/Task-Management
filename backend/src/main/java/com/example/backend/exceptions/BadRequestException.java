fed599d9b580c0dadb7c450b8c116bd70a8e261c
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-11 12:54:18 +0200

    added 2 exception classes

---
+++ backend/src/main/java/com/example/backend/exceptions/BadRequestException.java
@@ -0,0 +1,5 @@
+package com.example.backend.exceptions;
+
+public class BadRequestException extends RuntimeException {
+    // Add any additional properties or constructors if needed
+}
---
+++ backend/src/main/java/com/example/backend/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,6 @@
+package com.example.backend.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    // Add any additional properties or constructors if needed
+}
+
