826281cc1b58b9573cf957e62d9ba73b6f830caf
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-11 13:04:03 +0200

    added GlobalExceptionHandler

---
+++ backend/src/main/java/com/example/backend/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.example.backend.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        // Creates a custom error message or payload
+        String errorMessage = "Resource not found";
+
+        // Returns a ResponseEntity with the error message and appropriate HTTP status
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<String> handleBadRequestException(BadRequestException ex) {
+        // Creates a custom error message or payload
+        String errorMessage = "Bad request";
+
+        // Returns a ResponseEntity with the error message and appropriate HTTP status
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        // Create a custom error message or payload
+        String errorMessage = "Internal server error";
+
+        // Return a ResponseEntity with the error message and appropriate HTTP status
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+    }
+}
