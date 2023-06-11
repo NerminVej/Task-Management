60a281059e1d269df84dfce232e190b0add088d6
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-11 10:20:48 +0200

    added mroe configuration

--- backend/src/main/java/com/example/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,6 +16,33 @@ public class SecurityConfig extends WebSecurityConfiguration {
 
     public SecurityConfig(UserDetailsService userDetailsService) {
         this.userDetailsService = userDetailsService;
+    }
+
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeExchange()
+                .pathMatchers("/public/**").permitAll() // Allow public access to certain URLs
+                .pathMatchers("/admin/**").hasRole("ADMIN") // Restrict access to "/admin/**" URLs to users with the "ADMIN" role
+                .anyExchange().authenticated() // Require authentication for any other request
+                .and()
+                .formLogin()
+                // Specifies the custom login page URL
+                .loginPage("/login")
+                // Allows access to the login page
+                .permitAll()
+                .and()
+                .logout()
+                // Specifies the logout URL
+                .logoutUrl("/logout")
+                // Specifies the redirect URL after successful logout
+                .logoutSuccessUrl("/login?logout")
+                // Invalidates the session upon logout
+                .invalidateHttpSession(true)
+                // Deletes the JSESSIONID cookie upon logout
+                .deleteCookies("JSESSIONID");
     }
 
 
