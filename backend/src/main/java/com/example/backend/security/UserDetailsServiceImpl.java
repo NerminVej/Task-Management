40462045ee3b8e59e2729b46ca985baf5ba999f3
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-11 09:47:14 +0200

    added security folder

---
+++ backend/src/main/java/com/example/backend/security/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.example.backend.security;
+
+import com.example.backend.models.User;
+import com.example.backend.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository; // Inject your user repository
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        // Create and return an instance of UserDetails with the user details
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRoles().toArray(new String[0]))
+                .build();
+    }
+}
