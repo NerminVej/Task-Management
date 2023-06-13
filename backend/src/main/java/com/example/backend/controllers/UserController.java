88b889c04840af83329f563e0a7c896a93998b8a
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-13 08:32:52 +0200

    added new endpoints

--- backend/src/main/java/com/example/backend/controllers/UserController.java
+++ backend/src/main/java/com/example/backend/controllers/UserController.java
@@ -1,9 +1,11 @@
 package com.example.backend.controllers;
 
+import com.example.backend.models.Task;
 import com.example.backend.models.User;
+import com.example.backend.repositories.TaskRepository;
+import com.example.backend.repositories.UserRepository;
 import com.example.backend.services.UserService;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,10 +18,18 @@ import java.util.Optional;
 public class UserController {
 
     private final UserService userService;
+
+
+
+    private final UserRepository userRepository;
+    private final TaskRepository taskRepository;
     //@Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, UserRepository userRepository, TaskRepository taskRepository) {
         this.userService = userService;
+        this.userRepository = userRepository;
+        this.taskRepository = taskRepository;
     }
+
 
     @GetMapping("/")
     public ResponseEntity<List<User>> getAllUsers() {
@@ -38,12 +48,44 @@ public class UserController {
         }
     }
 
+    // Get all tasks for a specific user
+    @GetMapping("/{userId}/tasks")
+    public ResponseEntity<List<Task>> getUserTasks(@PathVariable Long userId) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            List<Task> tasks = user.getTasks();
+            return new ResponseEntity<>(tasks, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
 
 
     @PostMapping("/")
     public ResponseEntity<User> registerUser(@Valid @RequestBody User user) {
         User createdUser = userService.registerUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+
+    // Create a new task for a specific user
+    @PostMapping("/{userId}/tasks")
+    public ResponseEntity<Task> createTask(@PathVariable Long userId, @RequestBody Task task) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            task.setUser(user); // Associate the task with the user
+
+            Task savedTask = taskRepository.save(task);
+            user.getTasks().add(savedTask); // Add the task to the user's task list
+            userRepository.save(user);
+
+            return new ResponseEntity<>(savedTask, HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @DeleteMapping("/{id}")
